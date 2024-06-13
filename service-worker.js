eb17007faa00e41d54045b0a072d5ac84c209304
natalia <natakarelskaya@yandex.ru>
2024-06-13 17:01:31 +0400

    test only mob

--- service-worker.js
+++ service-worker.js
@@ -1,49 +1,58 @@
-const CACHE_NAME = 'y-cache';
+const CACHE_NAME = "my-cache";
 const urlsToCache = [
-  'https://natalia-orlova.github.io/btnUp/',
-  'https://natalia-orlova.github.io/btnUp/index.html',
-  'https://natalia-orlova.github.io/btnUp/style.css',
-  'https://natalia-orlova.github.io/btnUp/script.js',
-  'https://natalia-orlova.github.io/btnUp/images/icon-192x192.png'
+  "https://natalia-orlova.github.io/btnUp/",
+  "https://natalia-orlova.github.io/btnUp/index.html",
+  "https://natalia-orlova.github.io/btnUp/style.css",
+  "https://natalia-orlova.github.io/btnUp/script.js",
+  "https://natalia-orlova.github.io/btnUp/images/icon-192x192.png",
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/styles.css",
+        "/script.js",
+        "/images/icon-192x192.png",
+      ]);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.match(event.request)
-          .then(response => {
-            return response || fetch(event.request);
-          })
-      })
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
   );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.filter(cacheName => cacheName !== CACHE_NAME).map(cacheName => caches.delete(cacheName))
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
       );
     })
   );
 });
 
-self.addEventListener('appinstalled', () => {
+self.addEventListener("appinstalled", () => {
   // Скрыть кнопку после установки приложения
-  document.querySelector('.install-btn').style.display = 'none';
+  document.querySelector(".install-btn").style.display = "none";
 });
 
-self.addEventListener('beforeinstallprompt', (event) => {
+self.addEventListener("beforeinstallprompt", (event) => {
   event.preventDefault();
-  localStorage.setItem('appInstallCancelled', 'true');
+  localStorage.setItem("appInstallCancelled", "true");
 });
