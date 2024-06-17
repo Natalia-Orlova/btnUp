7871c1e0bbb070e3f2066fbebcd7ba2fc444f8bc
natalia <natakarelskaya@yandex.ru>
2024-06-17 16:35:02 +0400

    labels pwa

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
 const CACHE_NAME = "my-cache";
 const urlsToCache = [
-  // "https://natalia-orlova.github.io/btnUp/",
-  "https://natalia-orlova.github.io/btnUp/index.html",
-  "https://natalia-orlova.github.io/btnUp/style.css",
-  "https://natalia-orlova.github.io/btnUp/script.js",
+  "https://natalia-orlova.github.io/btnUp/",
+  // "https://natalia-orlova.github.io/btnUp/index.html",
+  // "https://natalia-orlova.github.io/btnUp/style.css",
+  // "https://natalia-orlova.github.io/btnUp/script.js",
   "https://natalia-orlova.github.io/btnUp/images/icon-192x192.png",
 ];
 
